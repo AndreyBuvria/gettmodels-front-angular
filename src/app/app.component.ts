b3b5e3e1781af2f7755e8bde1bc0f1f3c65b4684
Valerii1984 <mingaev.1984@gmail.com>
2025-04-29 18:32:14 +0300

    Fix: header layout adjustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PageLayoutComponent } from "./common/page-layout/page-layout.component";
-import { AppHeaderComponent } from "./common/app-header/app-header.component";
+import { PageLayoutComponent } from './common/page-layout/page-layout.component';
+import { AppHeaderComponent } from './common/app-header/app-header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, PageLayoutComponent, AppHeaderComponent],
-  template: `
-    <app-page-layout>
-      <div header>
-        <app-header></app-header>
-      </div>
-      
-      <div body>
-        <router-outlet></router-outlet>
-      </div>
-      
-      <!-- Footer будет добавлен позже вашим коллегой -->
-    </app-page-layout>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'gettmodels';
+  
 }
--- src/app/common/user-avatar/user-avatar.component.ts
+++ src/app/common/user-avatar/user-avatar.component.ts
@@ -7,6 +7,6 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./user-avatar.component.scss']
 })
 export class UserAvatarComponent {
-  @Input() avatarUrl: string = 'assets/images/7a049a671d198491f899b5aac9fae7014d7ba926.jpg'; // URL аватара
-  @Input() username: string = 'Denis 3D'; // Имя пользователя
+  @Input() avatarUrl: string = 'assets/images/user-avatar.jpg'; 
+  @Input() username: string = 'Denis 3D'; 
 }
